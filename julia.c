1de270b4943651ee7e79def24a366fd4b6203510
jeluiz4 <jeffluiz97@gmai.com>
2022-11-23 19:53:37 -0300

    mac_makefile_julia_update

--- julia.c
+++ julia.c
@@ -6,7 +6,7 @@
 /*   By: jeluiz4 <jeffluiz97@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 11:13:27 by jeluiz4           #+#    #+#             */
-/*   Updated: 2022/11/17 20:29:11 by jeluiz4          ###   ########.fr       */
+/*   Updated: 2022/11/23 16:42:50 by jeluiz4          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,8 @@ double	mandelb(double *zr, double *zi, double y, double x)
 	zi2 = *zi * *zi;
 	while ((i < MAX_IT) && ((zr2 + zi2) < 4))
 	{
-		*zi = 2.0 * *zr * *zi + (0.3842);
-		*zr = zr2 - zi2 + (- 0.70176);
+		*zi = 2.0 * *zr * *zi + (0.8);
+		*zr = zr2 - zi2 + (0);
 		zr2 = *zr * *zr;
 		zi2 = *zi * *zi;
 		i++;
@@ -69,8 +69,8 @@ void	ft_mb(t_data img, double x, double y, double color)
 				my_mlx_pp(&img, x, y, create_trgb(0, 0, 0, 0));
 			else
 			{
-				color = (color * 255 / MAX_IT) * 84 * 42;
-				my_mlx_pp(&img, x, y, create_trgb(0, color, 0, 255));
+				color = (color * 255 / MAX_IT);
+				my_mlx_pp(&img, x, y, create_trgb(0, color, color, color));
 			}
 			x++;
 		}
@@ -80,15 +80,46 @@ void	ft_mb(t_data img, double x, double y, double color)
 
 int	ft_close(int keycode, t_win *vars)
 {
-	mlx_destroy_window(vars->mlx, vars->win);
-	exit (13);
+	if (keycode == ESC_BUT)
+	{
+		//mlx_destroy_image(vars->mlx, vars->img);
+		mlx_destroy_window(vars->mlx, vars->win);
+		exit (13);
+	}
+	return (0);
+}
+
+int	ch_ose(int key, t_win *vars)
+{
+	if (key == A_UP)
+		printf("KEYPRESSED A_UP == %d\n", A_UP);
+	if (key == A_DOWN)
+		printf("KEYPRESSED A_DOWN == %d\n", A_DOWN);
+	if (key == A_RIGHT)
+		printf("KEYPRESSED A_RIGHT == %d\n", A_RIGHT);
+	if (key == A_LEFT)
+		printf("KEYPRESSED A_LEFT == %d\n", A_LEFT);
+	if (key == C_BUT)
+		printf("KEYPRESSED C_BUT == %d\n", C_BUT);
+	if (key == H_BUT)
+		printf("KEYPRESSED H_BUT == %d\n", H_BUT);
+	if (key == R_BUT)
+		printf("KEYPRESSED R_BUT == %d\n", R_BUT);
+	if (key == ESC_BUT)
+	{
+		printf("KEYPRESSED ESC_BUT == %d\n", ESC_BUT);
+		ft_close(key, vars);
+	}
 }
 
 int	main(void)
 {
 	t_win	mlx;
 	t_data	img;
+	t_all	bgm;
 
+	bgm.vars.z = 5.5;
+	printf("Será ? %.2f\n", bgm.vars.z);
 	mlx.mlx = mlx_init();
 	mlx.win = mlx_new_window(mlx.mlx, WIDTH, HEIGHT, "Fract-oil");
 	img.img = mlx_new_image(mlx.mlx, WIDTH, HEIGHT);
@@ -96,6 +127,6 @@ int	main(void)
 			&img.endian);
 	ft_mb(img, 0.0, 0.0, 0.0);
 	mlx_put_image_to_window(mlx.mlx, mlx.win, img.img, 0, 0);
-	mlx_hook(mlx.win, 2, ESC_BUT, ft_close, &mlx);
+	mlx_key_hook(mlx.win, &ch_ose, &mlx);
 	mlx_loop(mlx.mlx);
 }
